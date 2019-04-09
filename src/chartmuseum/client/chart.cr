ba8fae30486f455b8e44a4a490915c1c5296b57c
Josh Dvir <josh@dvir.us>
2019-04-08 23:28:16 -0400

    better docs

--- src/chartmuseum/client/chart.cr
+++ src/chartmuseum/client/chart.cr
@@ -6,7 +6,6 @@ module Chartmuseum
     module Chart
       # Gets a list of all charts.
       #
-      # - option param [String] chart The name of a chart.
       # - return [JSON::Any]  List of all versions of a chart.
       #
       # ```
@@ -18,26 +17,26 @@ module Chartmuseum
 
       # Gets a list of all versions of a chart.
       #
-      # - option param [String] chart The name of a chart.
+      # - chart [String] The name of a chart.
       # - return [JSON::Any]  List of all versions of a chart.
       #
       # ```
       # client.charts("application_x")
       # ```
-      def chart(chart : String? = nil) : JSON::Any
+      def chart(chart : String) : JSON::Any
         get("/api/charts/#{chart}").parse
       end
 
       # Gets a version of chart.
       #
-      # - option param [String] chart The name of a chart.
-      # - option param [String] version The name of a chart.
+      # - chart [String] The name of a chart.
+      # - version [String] The name of a chart.
       # - return [JSON::Any]  List of all versions of a chart.
       #
       # ```
       # client.verion("application_x", "version_1")
       # ```
-      def version(chart : String? = nil, version : String? = nil) : JSON::Any
+      def version(chart : String, version : String) : JSON::Any
         get("/api/charts/#{chart}/#{version}").parse
       end
     end
